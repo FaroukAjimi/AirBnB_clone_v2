54dba435cbb18ca5729225df44301fcc523a8fb8
FaroukAjimi <benajimi@gmail.com>
2020-04-09 22:46:01 +0100

    PERP

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+sudo mkdir -p /data/web_static/releases/test/
+sudo bash -c 'echo """<html>
+ <head>
+ </head>
+ <body>
+   Holberton School
+ </body>
+</html>""" > /data/web_static/releases/test/index.html'
+ln -fs  /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "46i location /hbnb_static{\nalias /data/web_static/current/;\n}" /etc/nginx/nginx.conf
+sudo service nginx restart
