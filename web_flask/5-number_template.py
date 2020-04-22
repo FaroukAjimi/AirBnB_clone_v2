52b090e1ca651c24053f6fbb66665e1a89d9f2ff
FaroukAjimi <benajimi@gmail.com>
2020-04-22 20:46:12 +0100

    import rendertemplate

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from flask import Flask, escape, request, abort
+from flask import Flask, escape, request, abort, render_template
 
 app = Flask(__name__)
 
@@ -38,6 +38,10 @@ def integer(n):
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def render(n):
-    return render_template('5-number.html', n=n)
+    try:
+        int(n)
+        return render_template('5-number.html', n=n)
+    except:
+        abort(404)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
