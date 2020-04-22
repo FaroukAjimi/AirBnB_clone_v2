28ac13005241551a5f1bc8f5ce2f0cde77f40c31
FaroukAjimi <benajimi@gmail.com>
2020-04-22 13:20:23 +0100

    variable env: replace

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,6 +17,7 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def var(text):
+    text = text.replace("_", " ")
     return "C " + text
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
